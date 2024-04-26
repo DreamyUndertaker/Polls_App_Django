41afbca1772cc908dc90f58eab102387fdef11d0
DreamyUndertaker <vadiimreg73@gmail.com>
2024-04-26 10:46:42 +0400

    Some cosmetic and code change

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 
 from home.models import Instructions
 
-# Register your models here.
 admin.site.register(Instructions)
--- home/models.py
+++ home/models.py
@@ -2,11 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 
-# Create your models here.
 class Instructions(models.Model):
     title = models.CharField(max_length=30)
     file = models.FileField(upload_to='instructions/', default=None, null=False, blank=False)
-    # TODO make fileField
 
     def get_absolute_url(self):
         return reverse("instruction_detail", kwargs={"pk": self.pk})
